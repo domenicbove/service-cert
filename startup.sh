411276fe5bf95cece768d3c74bd80e198c900fc7
Domenic <domenicbove@gmail.com>
2017-11-10 16:29:19 -0800

    extra spaces being hte culprit?

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 openssl pkcs12 -export -inkey /etc/amq-certificate-volume/tls.key -in /etc/amq-certificate-volume/tls.crt -out /etc/certs/tls.p12 -password pass:fisopenshift
 
 # Generate the keystore
-keytool -importkeystore  -srckeystore /etc/certs/tls.p12 -srcstoretype pkcs12 -destkeystore /etc/amq-secret-volume/broker.ks -storepass fisopenshift  -srcstorepass fisopenshift
+keytool -importkeystore -srckeystore /etc/certs/tls.p12 -srcstoretype pkcs12 -destkeystore /etc/amq-secret-volume/broker.ks -storepass fisopenshift -srcstorepass fisopenshift
 
 # Generate the truststore
 keytool -import -keystore /etc/amq-secret-volume/broker.ts -file /etc/amq-certificate-volume/tls.crt -storepass fisopenshift -alias broker
