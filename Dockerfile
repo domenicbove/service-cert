2e8408e8b5dd4e33acbb5aa1e2277075594123de
Domenic <domenicbove@gmail.com>
2017-11-10 16:08:12 -0800

    minor improvements, env var may work in the end and can revert

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,23 @@
 FROM registry.access.redhat.com/jboss-amq-6/amq63-openshift
+
 MAINTAINER Domenic Bove <dbove@redhat.com>
+
 USER root
+
 RUN yum install -y openssl && \
     yum clean all
+
 COPY startup.sh /etc/scripts/startup.sh
-RUN chgrp -R 0 /etc/scripts/ && \
+
+RUN chmod -R u+x /etc/scripts/ && \
+    chgrp -R 0 /etc/scripts/ && \
     chmod -R g=u /etc/scripts/
+
+RUN mkdir /etc/certs/ && \
+    chmod -R u+x /etc/certs/ && \
+    chgrp -R 0 /etc/certs/ && \
+    chmod -R g=u /etc/certs/
+
 USER 1001
-CMD ["/etc/scripts/startup.sh"]
+
+CMD . /etc/scripts/startup.sh
--- startup.sh
+++ startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Generate the pkcs12 file
-openssl  pkcs12 -export -inkey $KEY_LOCATION -in $CERT_LOCATION -out /etc/certs/tls.p12 -password pass:$STORE_PASS
+openssl pkcs12 -export -inkey /etc/amq-certificate-volume/tls.key -in /etc/amq-certificate-volume/tls.crt -out /etc/certs/tls.p12 -password pass:fisopenshift
 
 # Generate the keystore
-keytool -importkeystore  -srckeystore /etc/certs/tls.p12 -srcstoretype pkcs12 -destkeystore $AMQ_KEYSTORE_TRUSTSTORE_DIR/$AMQ_KEYSTORE -storepass $AMQ_KEYSTORE_PASSWORD  -srcstorepass $AMQ_KEYSTORE_PASSWORD
+keytool -importkeystore  -srckeystore /etc/certs/tls.p12 -srcstoretype pkcs12 -destkeystore /etc/amq-secret-volume/broker.ks -storepass fisopenshift  -srcstorepass fisopenshift
 
 # Generate the truststore
-keytool -import -keystore $AMQ_KEYSTORE_TRUSTSTORE_DIR/$AMQ_TRUSTSTORE -file $CERT_LOCATION -storepass $AMQ_TRUSTSTORE_PASSWORD -alias broker
+keytool -import -keystore /etc/amq-secret-volume/broker.ts -file /etc/amq-certificate-volume/tls.crt -storepass fisopenshift -alias broker
 
 # TODO figure out how to pass in the original startup script
 /opt/amq/bin/launch.sh
